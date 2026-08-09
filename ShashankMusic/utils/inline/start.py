634846f04b39bb01c504916c7cd73cb2fe5a671b
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2026-08-09 17:46:11 +0530

    Update start.py

--- ShashankMusic/utils/inline/start.py
+++ ShashankMusic/utils/inline/start.py
@@ -1,7 +1,15 @@
 # -----------------------------------------------
-# 🔸 DG Ravan Bhai Project
-# 🔹 Developed & Maintained by: DG Ravan Bhai
-# 📅 Copyright © 2026 – All Rights Reserved
+# 🔸 ShashankMusic Project
+# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# 📅 Copyright © 2025 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzShukla
 # -----------------------------------------------
 
 import random
@@ -30,7 +38,7 @@ def start_panel(_):
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
-                url="https://t.me/+2tT6e0fT84dmMjA9",
+                url=config.SUPPORT_CHAT,
                 style=group_style
             ),
         ],
@@ -66,20 +74,13 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_2"], 
-                url="https://t.me/+2tT6e0fT84dmMjA9",
+                url=config.SUPPORT_CHAT,
                 style=group_style
             ),
             InlineKeyboardButton(
                 text=_["S_B_6"], 
-                url="https://t.me/Il_Ravan_bhai_ll",
+                url=config.SUPPORT_CHANNEL,
                 style=group_style
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="Developer",
-                url=config.DEVELOPER_URL,
-                style=alone_style
             ),
         ],
         [
